12ee596a366beeeac20f234fb16d1586a15ce4b9
Haj Rezvan <rezvan1379@aut.ac.ir>
2020-08-15 20:50:46 +0430

    Create a new class for saving information of groups and persons.

---
+++ app/src/main/java/com/example/booaplication/Model/Person.java
@@ -0,0 +1,70 @@
+package com.example.booaplication.Model;
+
+public class Person {
+    private int id;
+    private String category;
+    private String name;
+    private String field;
+    private String disc;
+    private String image;
+    private int favorite;
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getDisc() {
+        return disc;
+    }
+
+    public void setDisc(String disc) {
+        this.disc = disc;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(int favorite) {
+        this.favorite = favorite;
+    }
+}
