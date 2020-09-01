183aa5a69ae38e9e2705eb3300965468d0eadb36
Haj Rezvan <rezvan1379@aut.ac.ir>
2020-09-01 13:41:28 +0430

    Class changed.

--- app/src/main/java/com/example/booaplication/KharejFragment.java
+++ app/src/main/java/com/example/booaplication/KharejFragment.java
@@ -8,20 +8,44 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.booaplication.Adapter.PersonAdapter;
+import com.example.booaplication.DataBase.Database;
+import com.example.booaplication.Model.Person;
+
+import java.io.IOException;
+import java.util.List;
 
 public class KharejFragment extends Fragment {
 
     private View view;
+    private RecyclerView recyclerView;
+    private PersonAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.recycler_home, container, false);
+        try {
+            setupView();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return view;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+    public void setupView() throws IOException {
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_id);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        Database database = new Database(getActivity());
+        List<Person> personList = database.getForeignPerson();
+        adapter = new PersonAdapter(getActivity(), personList);
+        recyclerView.setAdapter(adapter);
     }
 }
