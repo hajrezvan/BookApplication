a83a1e89d1e5044d01d957a2e9eb00350379bc41
Haj Rezvan <rezvan1379@aut.ac.ir>
2020-09-01 14:04:15 +0430

    Add name of user to program.

--- app/src/main/java/com/example/booaplication/ProfileFragment.java
+++ app/src/main/java/com/example/booaplication/ProfileFragment.java
@@ -1,9 +1,13 @@
 package com.example.booaplication;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,17 +16,28 @@ import androidx.fragment.app.Fragment;
 public class ProfileFragment extends Fragment {
 
     private View view;
+    private TextView name;
+    private String username;
+    public static final String LOGIN = "Login";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.profile_fragment, container, false);
-        String username = view.findViewById(R.id.username_profile_id).toString();
+        setupViews();
         return view;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @SuppressLint("CutPasteId")
+    public void setupViews() {
+        name = view.findViewById(R.id.username_profile_id);
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(LOGIN, Context.MODE_PRIVATE);
+        username = sharedPreferences.getString("NAME", null);
+        name.setText(username);
     }
 }
